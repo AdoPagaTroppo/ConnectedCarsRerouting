4ba3cc8b4806384baed02a536145507d6f85325f
AdoPagaTroppo <A.PAGANO93@studenti.unisa.it>
2024-10-06 10:01:15 +0200

    Updated spawners for realistic scenario

--- algorithm_animation.py
+++ algorithm_animation.py
@@ -11,11 +11,12 @@ import time
 from behaviours_maker import calculate_pathlen
 
 # decomment the starting edge from which you want the animation to start
-# start = '766350967' # start edge of cars in Salerno city centre map, located in Concordia parking area
-start = '-79867013#1' # start edge of cars in Unisa map, located in Multipiano parking area
+start = '766350967' # start edge of cars in Salerno city centre map, located in Concordia parking area
+# start = '-79867013#1' # start edge of cars in Unisa map, located in Multipiano parking area
 # end = '50827474' # end edge of cars in Salerno city centre map, direction towards Avellino
-end = '-40567772#0' # end edge of cars in Unisa map, direction towards Avellino
+# end = '-40567772#0' # end edge of cars in Unisa map, direction towards Avellino
 # end = '93450829' # end edge of cars in Salerno city centre map, direction towards Napoli
+end = '102235300#2' # end edge of cars in Salerno city centre map, direction towards other cities in the province of Salerno
 SCENARIO = 'Salerno'
 dijkstrabased = {} # data structure to keep track of first edges chosen by Dijkstra algorithm for paths
 mapdata = MapData(SCENARIO)
@@ -27,7 +28,8 @@ for i in range(4):
     paths[i] = index2path(i,end,start,mapdata,works=works)
     if i == 0:
         dijkstrabased[start.getID()] = paths[i][1] # update data structure to keep track of first edges chosen by Dijkstra algorithm for paths
-    colorv = getIfromRGB(list(alg_color(index2alg(i)))[0:3])          
+    colorv = getIfromRGB(list(alg_color(index2alg(i)))[0:3])        
+    print(calculate_pathlen(paths[i],works,mapdata))  
     # color paths     
     for e in paths[i]:
         traci.edge.setParameter(e,'color',colorv)
